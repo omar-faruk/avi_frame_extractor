cb4fa3f9df3f9498c28988111383a458861c7adf
omar-faruk <munna93_bd@yahoo.com>
2019-05-11 00:30:35 +0600

    reading jpeg frames by tracing start and end signature bytes

--- read_avi.c
+++ read_avi.c
@@ -27,11 +27,26 @@ typedef struct AVI_HEADER
 	uint32_t size_of_data_chunk;
 }AVI_HEADER;
 
+typedef struct _JFIFHeader
+{
+  uint8_t SOI[2];          /* 00h  Start of Image Marker     */
+  uint8_t APP0[2];         /* 02h  Application Use Marker    */
+  uint8_t Length[2];       /* 04h  Length of APP0 Field      */
+  uint8_t Identifier[5];   /* 06h  "JFIF" (zero terminated) Id String */
+  uint8_t Version[2];      /* 07h  JFIF Format Revision      */
+  uint8_t Units;           /* 09h  Units used for Resolution */
+  uint8_t Xdensity[2];     /* 0Ah  Horizontal Resolution     */
+  uint8_t Ydensity[2];     /* 0Ch  Vertical Resolution       */
+  uint8_t XThumbnail;      /* 0Eh  Horizontal Pixel Count    */
+  uint8_t YThumbnail;      /* 0Fh  Vertical Pixel Count      */
+} JFIFHEAD;
+
 int main()
 {
 	FILE * fp;
 	RIFF_HEADER  head;
 	AVI_HEADER avi_head;
+	uint8_t soi[2];
 	
 	fp = fopen("out.avi","r");
 	
@@ -45,4 +60,35 @@ int main()
 	printf("size of data chunks: %u\n",avi_head.size_of_data_chunk);
 	printf("AVI data rate: %u bps\n",avi_head.data_rate_avi);
 	printf("Data Rate: %u\n",avi_head.data_rate);
+
+	FILE * frame;
+
+	char frame_name[50];
+	uint8_t bytes[2];
+	int bytes_read, frame_count = 0;
+	int jpeg_start = 0;
+	
+	while(1){
+		bytes_read =  fread(bytes,1,2,fp);
+		if(bytes_read<2){
+			break;
+		}
+		if(bytes[0] == 0xFF && bytes[1]==0xD8){
+			jpeg_start=1;
+			memset(frame_name,0,50);
+			sprintf(frame_name,"Frame%d.jpg",frame_count);
+			frame_count++;
+			puts(frame_name);
+			frame = fopen(frame_name,"wb");
+		}
+
+		if(jpeg_start){
+			fwrite(bytes,1,2,frame);
+			if(bytes[0]==0xFF && bytes[1]==0xD9)
+			{
+				jpeg_start = 0;
+				fclose(frame);
+			}	
+		}
+	}
 }
